6883a58149ee0bb1a10f84226fa46f5c9211bfee
Skye <wangy2@myumanitoba.ca>
2021-02-15 00:23:57 -0600

    splash page made, run successfully

--- app/src/main/java/com/example/myapplication/Business/Access_task.java
+++ app/src/main/java/com/example/myapplication/Business/Access_task.java
@@ -1,4 +1,5 @@
 package com.example.myapplication.Business;
 
 public class Access_task {
+
 }
--- app/src/main/java/com/example/myapplication/SplashPageActivity.java
+++ app/src/main/java/com/example/myapplication/SplashPageActivity.java
@@ -1,4 +1,39 @@
 package com.example.myapplication;
 
-public class SplashPageActivity {
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static java.lang.Thread.sleep;
+
+public class SplashPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3600);
+                    Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(it);
+                    finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+            splashThread.start();
+    }
 }

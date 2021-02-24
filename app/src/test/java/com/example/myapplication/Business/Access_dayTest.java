1c51db1782aa34dae41a9660d5eff5583b9c37d8
yuangBI <biy1@myumanitoba.ca>
2021-02-25 06:45:12 +0800

    make change on test file

---
+++ app/src/test/java/com/example/myapplication/Business/Access_dayTest.java
@@ -0,0 +1,40 @@
+package com.example.myapplication.Business;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Access_dayTest {
+
+    @Test
+    public void setStartTime() {
+    }
+
+    @Test
+    public void setEndTime() {
+    }
+
+    @Test
+    public void setMonth() {
+    }
+
+    @Test
+    public void setDay() {
+    }
+
+    @Test
+    public void setYear() {
+    }
+
+    @Test
+    public void deleteDay() {
+    }
+
+    @Test
+    public void addDay() {
+    }
+
+    @Test
+    public void getAllDay() {
+    }
+}

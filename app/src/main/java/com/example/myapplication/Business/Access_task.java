c102d337c5bca190e0682c83f4c48e3819431435
emily0906 <trann34@myumanitoba.ca>
2021-02-24 12:46:08 -0600

    Change

--- app/src/main/java/com/example/myapplication/Business/Access_task.java
+++ app/src/main/java/com/example/myapplication/Business/Access_task.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Access_task extends AppCompatActivity {
 
     private List<Tasks> allTasks;
-    private final Task_persistence taskPersistence;
+    private Task_persistence taskPersistence;
 
     public Access_task(){
     // need a fake database here
@@ -22,16 +22,16 @@ public class Access_task extends AppCompatActivity {
         allTasks=new ArrayList<>();
     }
 
-    public Access_task(final Task_persistence taskPersistence){
+    public Access_task(Task_persistence taskPersistence){
         this.taskPersistence = taskPersistence;
         allTasks = taskPersistence.getAllTasks();
     }
 
-    public final Tasks getTask(int taskId) {
+    public Tasks getTask(int taskId) {
         return taskPersistence.getTask(taskId);
     }
 
-    public final Tasks addTask(final Tasks newTask){
+    public Tasks addTask(final Tasks newTask){
         if(newTask.getTaskTitle() == null || newTask.getTaskDescription() == null || newTask.getTaskDate() == null){ //if(newTask == null)
             String taskCreateMessage = "Please enter all fields.";
             Toast.makeText(getApplicationContext(), taskCreateMessage, Toast.LENGTH_SHORT).show();
@@ -39,15 +39,15 @@ public class Access_task extends AppCompatActivity {
         return taskPersistence.addTask(newTask);
     }
 
-    public final Tasks deleteTask(final Tasks taskToDel){
+    public Tasks deleteTask(Tasks taskToDel){
         return taskPersistence.deleteTask(taskToDel);
     }
 
-    public final void editTask(Tasks oldTask, Tasks newTask){
+    public void editTask(Tasks oldTask, Tasks newTask){
         taskPersistence.editTask(oldTask,newTask);
     }
 
-    public final List<Tasks> getAllTasks() {
+    public List<Tasks> getAllTasks() {
         allTasks = taskPersistence.getAllTasks();
         return allTasks;
     }
---
+++ app/src/main/java/com/example/myapplication/Presentation/TaskActivity.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.Presentation;
+
+public class TaskActivity {
+}

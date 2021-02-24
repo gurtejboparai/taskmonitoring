4b92767178ae8d4becd6a84305015348582e491a
boparai1999 <boparai3@myumanitoba.ca>
2021-02-23 20:19:30 -0600

    latest commit

--- app/src/main/java/com/example/myapplication/Presentation/DayActivity.java
+++ app/src/main/java/com/example/myapplication/Presentation/DayActivity.java
@@ -2,9 +2,16 @@ package com.example.myapplication.Presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class DayActivity extends AppCompatActivity {
 
@@ -12,5 +19,15 @@ public class DayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
+
+        String dateString=new SimpleDateFormat("MM dd, yyyy", Locale.getDefault()).format(new Date());
+        TextView date =findViewById(R.id.editTextDate);
+        date.setText(dateString);
     }
+    public void buttonAddTaskOnClick(){
+        Intent newTaskIntent = new Intent(DayActivity.this, TaskActivity.class);
+        DayActivity.this.startActivity(newTaskIntent);
+    }
+
+
 }

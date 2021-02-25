751b4a96d4f8d394040c663f64eb4257a39eceb4
yuangBI <biy1@myumanitoba.ca>
2021-02-26 01:38:52 +0800

    change test file

---
+++ app/src/test/java/com/example/myapplication/Object/Task_DayTest.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.Object;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task_DayTest {
+
+    @Test
+    public void getDay() {
+    }
+
+    @Test
+    public void getTask() {
+    }
+
+    @Test
+    public void getStartTime() {
+    }
+
+    @Test
+    public void getEndTime() {
+    }
+
+    @Test
+    public void setDeadline() {
+    }
+}

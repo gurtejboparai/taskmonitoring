65ac9ee17fae8e6f5f976d6bad1686e9c5b071f7
Skye <wangy2@myumanitoba.ca>
2021-02-25 04:21:17 -0600

    added tests of getStatus and checForSame

--- app/src/test/java/com/example/myapplication/Object/TasksTest.java
+++ app/src/test/java/com/example/myapplication/Object/TasksTest.java
@@ -10,10 +10,12 @@ public class TasksTest {
     public void testTasks()
     {
         Tasks newTask;
+        Tasks oldTask;
 
         System.out.println("\nStarting testTasks...");
 
-        newTask = new Tasks(0, "Task 1", "Task 1 Description","2021-02-25");
+        oldTask = new Tasks(0, "Task 0", "Task 0 Description","2021-02-24");
+        newTask = new Tasks(1, "Task 1", "Task 1 Description","2021-02-25");
         newTask.setTaskTitle("What's new");
         newTask.setTaskDescription("Some changes here");
         newTask.setTaskDate("2021-02-26");
@@ -24,7 +26,8 @@ public class TasksTest {
         assertEquals("Task 1",newTask.getTaskTitle());
         assertEquals("Task 1 Description",newTask.getTaskDescription());
         assertEquals("2021-02-25",newTask.getTaskDate());
-        //assertTrue("on going".equals(newTask.getStatus()));
+        assertEquals("on going",newTask.getStatus());
+        assertFalse(newTask.checkForSame(oldTask));
 
         System.out.println("Finished testTasks...");
 

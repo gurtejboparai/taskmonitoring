feb5e347e193ad11359729f9f5969dfe9f37418b
emily0906 <trann34@myumanitoba.ca>
2021-02-25 17:36:22 -0600

    add interface

---
+++ app/src/main/java/com/example/myapplication/Persistence/Interface_TD.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.Persistence;
+
+import com.example.myapplication.Object.Task_Day;
+
+public interface Interface_TD {
+    void setDeadline(Task_Day td, String start, String end);
+}

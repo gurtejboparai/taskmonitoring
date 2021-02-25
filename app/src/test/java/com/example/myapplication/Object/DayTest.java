cad043cf8c97f0e5b7c5e913da0d30049eb80e1e
yuangBI <biy1@myumanitoba.ca>
2021-02-26 01:38:10 +0800

    change test file

---
+++ app/src/test/java/com/example/myapplication/Object/DayTest.java
@@ -0,0 +1,32 @@
+package com.example.myapplication.Object;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DayTest {
+
+    @Test
+    public void getDay() {
+    }
+
+    @Test
+    public void getMonth() {
+    }
+
+    @Test
+    public void getYear() {
+    }
+
+    @Test
+    public void changeDay() {
+    }
+
+    @Test
+    public void changeMonth() {
+    }
+
+    @Test
+    public void changeYear() {
+    }
+}

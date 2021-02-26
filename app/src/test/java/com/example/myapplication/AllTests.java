0867eead76451695e89dea5432ec70debb727de2
emily0906 <trann34@myumanitoba.ca>
2021-02-25 20:49:34 -0600

    Test

---
+++ app/src/test/java/com/example/myapplication/AllTests.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+import com.example.myapplication.Business.Access_TD;
+import com.example.myapplication.Business.Access_dayTest;
+import com.example.myapplication.Business.Access_taskTest;
+import com.example.myapplication.Object.DayTest;
+import com.example.myapplication.Object.Task_DayTest;
+import com.example.myapplication.Object.TasksTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DayTest.class,
+        TasksTest.class,
+        Task_DayTest.class,
+        Access_taskTest.class,
+        Access_dayTest.class,
+        Access_TD.class
+
+})
+public class AllTests{
+
+}

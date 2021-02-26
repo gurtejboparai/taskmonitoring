31d2bb023fe6a2b70c2c5b0518dadca4c065629d
yuangBI <biy1@myumanitoba.ca>
2021-02-26 09:36:06 +0800

    add DayTest and Access_dayTest

--- app/src/test/java/com/example/myapplication/Business/Access_dayTest.java
+++ app/src/test/java/com/example/myapplication/Business/Access_dayTest.java
@@ -1,32 +1,102 @@
 package com.example.myapplication.Business;
 
+import com.example.myapplication.Object.Day;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class Access_dayTest {
+    Access_day testAccessDay;
+    Day testDay;
+    Day testDay2;
+
+    @Before
+    public void setUp(){
+        System.out.println("\n\tStart the test for Access_day class\n\n");
+            testAccessDay=new Access_day();
+            testDay=new Day(12,10,2021);
+            testDay2=new Day(3,2,1900);
+            testAccessDay.addDay(testDay);
+
+    }
 
     @Test
     public void setMonth() {
+        System.out.println("\nStart the test for Access_day.setMonth() \n");
+
+            testAccessDay.setMonth(testDay,6);
+            assertEquals("change month from 10 to 6 fail",testDay.getMonth(),6);
+            testAccessDay.setMonth(testDay,11);
+            assertEquals("set month to 11 fail",testDay.getMonth(),11);
+            testAccessDay.setMonth(testDay,10);
+            assertEquals("set month back to 10 fail",testDay.getMonth(),10);
+
+        System.out.println("\nEnd the test for Access_day.setMonth() \n");
     }
 
     @Test
     public void setDay() {
+        System.out.println("\nStart the test for Access_day.setDay() \n");
+
+            testAccessDay.setDay(testDay,29);
+            assertEquals("change day from 12 to 29 fail",testDay.getDay(),29);
+            testAccessDay.setDay(testDay,1);
+            assertEquals("set day to 1 fail",testDay.getDay(),1);
+            testAccessDay.setDay(testDay,12);
+            assertEquals("set day back to 12 fail",testDay.getDay(),12);
+
+        System.out.println("\nEnd the test for Access_day.setDay() \n");
     }
 
     @Test
     public void setYear() {
+        System.out.println("\nStart the test for Access_day.setYear() \n");
+            testAccessDay.setYear(testDay,1999);
+            assertEquals("change year from 2021 to 1999 fail",testDay.getYear(),1999);
+            testAccessDay.setYear(testDay,7777);
+            assertEquals("set year to 7777 fail",testDay.getYear(),7777);
+
+        System.out.println("\nEnd the test for Access_day.setYear() \n");
     }
 
     @Test
     public void deleteDay() {
+        System.out.println("\nStart the test for Access_day.deleteDay() \n");
+            int temp=testAccessDay.getAllDay().indexOf(testDay);
+            testAccessDay.addDay(testDay2);
+            testAccessDay.deleteDay(testDay);
+            assertNotEquals("after delete day from list it still there",testAccessDay.getAllDay().indexOf(testDay),temp);
+            temp=testAccessDay.getAllDay().indexOf(testDay2);
+            testAccessDay.deleteDay(testDay2);
+            assertNotEquals("after delete day2 from list it still there",testAccessDay.getAllDay().indexOf(testDay2),temp);
+
+        System.out.println("\nEnd the test for Access_day.deleteDay() \n");
     }
 
     @Test
     public void addDay() {
+        System.out.println("\nStart the test for Access_day.addDay() \n");
+            testAccessDay.addDay(testDay);
+            testAccessDay.addDay(testDay2);
+            assertNotEquals("fail to add day in list",testAccessDay.getAllDay().indexOf(testDay),0);
+            assertNotEquals("fail to add day2 in list",testAccessDay.getAllDay().indexOf(testDay2),0);
+        System.out.println("\nEnd the test for Access_day.addDay() \n");
     }
 
     @Test
     public void getAllDay() {
+        System.out.println("\nStart the test for Access_day.getAllDay() \n");
+
+        assertNotNull("return null from getAllDay", testAccessDay.getAllDay());
+        assertEquals("not return correct day list",testAccessDay.getAllDay().indexOf(testDay),testAccessDay.getAllDay().indexOf(testDay));
+
+        System.out.println("\nEnd the test for Access_day.getAllDay() \n");
+    }
+    @After
+    public void end(){
+        System.out.println("\n\tEnd the test for Access_day class\n\n");
     }
 }
--- app/src/test/java/com/example/myapplication/Object/DayTest.java
+++ app/src/test/java/com/example/myapplication/Object/DayTest.java
@@ -1,32 +1,98 @@
 package com.example.myapplication.Object;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DayTest {
+    public Day testDay,testDay2;
+
+    @Before
+    public void setUp(){
+        System.out.println("\n\tStart the test for Day class\n\n");
+
+        testDay=new Day(12,10,2021);
+        testDay2=new Day(30,6,1990);
+    }
 
     @Test
     public void getDay() {
+        System.out.println("\nstart: test Day.getDay()\n");
+
+
+        assertEquals("test Day.getDay() fail, it not return correct day value",testDay.getDay(),12);
+        assertEquals("test Day.getDay() fail, it not return correct day value",testDay2.getDay(),30);
+        assertNotEquals("test Day.getDay() fail, it not return correct day value",testDay.getDay(),10);
+        assertNotEquals("test Day.getDay() fail, it not return correct day value",testDay.getDay(),2021);
+        assertNotEquals("test Day.getDay() fail, it not return correct day value",testDay.getDay(),0);
+
+        System.out.println("\nend: test Day.getDay()\n");
     }
 
     @Test
     public void getMonth() {
+        System.out.println("\nstart: test Day.getMonth()\n");
+
+        assertEquals("test Day.getMonth() fail, it not return correct month value",testDay.getMonth(),10);
+        assertEquals("test Day.getMonth() fail, it not return correct month value",testDay2.getMonth(),6);
+        assertNotEquals("test Day.getMonth() fail, it not return correct month value",testDay.getMonth(),12);
+        assertNotEquals("test Day.getMonth() fail, it not return correct month value",testDay.getMonth(),2021);
+        assertNotEquals("test Day.getMonth() fail, it not return correct month value",testDay.getMonth(),0);
+
+        System.out.println("\nend: test Day.getMonth()\n");
     }
 
     @Test
     public void getYear() {
+        System.out.println("\nstart: test Day.getYear()\n");
+
+
+        assertEquals("test Day.getYear() fail, it not return correct Year value",testDay.getYear(),2021);
+        assertEquals("test Day.getYear() fail, it not return correct Year value",testDay2.getYear(),1990);
+        assertNotEquals("test Day.getYear() fail, it not return correct Year value",testDay.getYear(),12);
+        assertNotEquals("test Day.getYear() fail, it not return correct Year value",testDay.getYear(),10);
+        assertNotEquals("test Day.getYear() fail, it not return correct Year value",testDay.getYear(),0);
+
+
+        System.out.println("\nend: test Day.getYear()\n");
+
     }
 
     @Test
     public void changeDay() {
+        System.out.println("\nstart: test Day.changeDay()\n");
+
+        testDay2.changeDay(30);
+        assertEquals("should change day to 30, but not work",testDay2.getDay(),30);
+
+
+
+        System.out.println("\nend: test Day.changeDay()\n");
     }
 
     @Test
     public void changeMonth() {
+        System.out.println("\nstart: test Day.changeMonth()\n");
+
+        testDay2.changeMonth(3);
+        assertEquals("should change month to 3, but not work",testDay2.getMonth(),3);
+
+        System.out.println("\nend: test Day.changeMonth()\n");
     }
 
     @Test
     public void changeYear() {
+        System.out.println("\nstart: test Day.changeYear()\n");
+
+        testDay2.changeYear(2014);
+        assertEquals("should change year to 2014, but not work",testDay2.getYear(),2014);
+
+        System.out.println("\nend: test Day.changeYear()\n");
+    }
+    @After
+    public void after(){
+        System.out.println("\n\n\t end the day test");
     }
 }

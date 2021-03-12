523d3f7d458f3596ab7da8d9ae18807314c8cdf1
emily0906 <trann34@myumanitoba.ca>
2021-03-12 17:58:29 -0600

    I change something

--- app/src/main/java/com/example/myapplication/application/Service.java
+++ app/src/main/java/com/example/myapplication/application/Service.java
@@ -1,13 +1,10 @@
 package com.example.myapplication.application;
 
 import com.example.myapplication.Persistence.Data.TaskPersistenceDB;
-import com.example.myapplication.Persistence.Data.DayPersistenceDB;
-import com.example.myapplication.Persistence.DayPersistence;
 import com.example.myapplication.Persistence.TaskPersistence;
 
 public class Service {
     private static TaskPersistence taskPersistence = null;
-    private static DayPersistence dayPersistence = null;
 
     public static synchronized TaskPersistence getTaskPersistence(){
         if(taskPersistence==null)
@@ -15,9 +12,4 @@ public class Service {
         return taskPersistence;
     }
 
-    public  static synchronized DayPersistence getDayPersistence(){
-        if(dayPersistence==null)
-            dayPersistence = new DayPersistenceDB();
-        return dayPersistence;
-    }
 }
--- app/src/test/java/com/example/myapplication/AllTests.java
+++ app/src/test/java/com/example/myapplication/AllTests.java
@@ -1,8 +1,5 @@
 package com.example.myapplication;
-import com.example.myapplication.Business.AccessDayTest;
 import com.example.myapplication.Business.AccessTaskTest;
-import com.example.myapplication.Object.DayTest;
-import com.example.myapplication.Object.TaskDayTest;
 import com.example.myapplication.Object.TaskTest;
 
 import org.junit.runner.RunWith;
@@ -10,12 +7,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        DayTest.class,
         TaskTest.class,
-        TaskDayTest.class,
         AccessTaskTest.class,
-        AccessDayTest.class
-        //Access_TD.class
 
 })
 public class AllTests{

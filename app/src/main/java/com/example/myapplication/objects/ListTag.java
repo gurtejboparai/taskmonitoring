4fe368ea46ae653184aa892d2542fbff89756ae7
emily0906 <trann34@myumanitoba.ca>
2021-03-15 17:03:58 -0500

    add Tag List class add constructors in Task to include tag name and deadline

--- app/src/main/java/com/example/myapplication/objects/ListTag.java
+++ app/src/main/java/com/example/myapplication/objects/ListTag.java
@@ -1,4 +1,50 @@
 package com.example.myapplication.objects;
+import android.graphics.Color;
+
+import androidx.annotation.ColorInt;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListTag {
+    List<Task>taskList;
+    @ColorInt int colour;
+
+    ListTag(String tagName){
+        taskList = new ArrayList<>();
+        if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.FITNESS))){
+            colour = Color.BLUE;
+        }
+        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.SCHOOL))){
+            colour=Color.RED;
+        }
+        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.WORK))){
+            colour = Color.YELLOW;
+        }
+        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.APPOINTMENT))){
+            colour = Color.MAGENTA;
+        }
+        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.PRODUCTIVITY))){
+            colour = Color.CYAN;
+        }
+        else
+            colour = Color.GREEN;
+    }
+
+    void addTask(Task newTask){
+        taskList.add(newTask);
+    }
+
+    void removeTask(Task rTask){
+        taskList.remove(rTask);
+    }
+
+    List<Task> getTaskList(){
+        return taskList;
+    }
+
+    int getColour(){
+        return colour;
+    }
+
 }

14516f8daddf8f2c1c8c6f99759e0ffa62dc3daf
emily0906 <trann34@myumanitoba.ca>
2021-03-15 17:10:38 -0500

    get and set deadline in task class

--- app/src/main/java/com/example/myapplication/objects/ListTag.java
+++ app/src/main/java/com/example/myapplication/objects/ListTag.java
@@ -9,26 +9,34 @@ import java.util.List;
 public class ListTag {
     List<Task>taskList;
     @ColorInt int colour;
+    TaskTag tag;
 
     ListTag(String tagName){
         taskList = new ArrayList<>();
         if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.FITNESS))){
             colour = Color.BLUE;
+            this.tag=TaskTag.FITNESS;
         }
         else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.SCHOOL))){
             colour=Color.RED;
+            this.tag=TaskTag.SCHOOL;
         }
         else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.WORK))){
             colour = Color.YELLOW;
+            this.tag = TaskTag.WORK;
         }
         else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.APPOINTMENT))){
             colour = Color.MAGENTA;
+            this.tag = TaskTag.APPOINTMENT;
         }
         else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.PRODUCTIVITY))){
             colour = Color.CYAN;
+            this.tag = TaskTag.PRODUCTIVITY;
         }
-        else
+        else {
             colour = Color.GREEN;
+            this.tag = TaskTag.MISLENIOUS;
+        }
     }
 
     void addTask(Task newTask){
@@ -42,6 +50,11 @@ public class ListTag {
     List<Task> getTaskList(){
         return taskList;
     }
+
+    TaskTag getTagList(){
+        return tag;
+    }
+
 
     int getColour(){
         return colour;

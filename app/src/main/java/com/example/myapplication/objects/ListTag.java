82acc1ca61b4615dd98499df432a91803855f5e5
emily0906 <trann34@myumanitoba.ca>
2021-03-15 21:02:30 -0500

    List tag modify

--- app/src/main/java/com/example/myapplication/objects/ListTag.java
+++ app/src/main/java/com/example/myapplication/objects/ListTag.java
@@ -7,29 +7,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListTag {
-    List<Task>taskList;
+    List<Task> schoolTasks;
+    List<Task> workTasks;
+    List<Task> fitnessTasks;
+    List<Task> appointmentTasks;
+    List<Task> productiveTask;
+    List<Task> taskList;//default task
     @ColorInt int colour;
     TaskTag tag;
 
-    ListTag(String tagName){
+    ListTag(TaskTag tagName){
         taskList = new ArrayList<>();
-        if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.FITNESS))){
+        productiveTask = new ArrayList<>();
+        appointmentTasks = new ArrayList<>();
+        fitnessTasks = new ArrayList<>();
+        schoolTasks = new ArrayList<>();
+        workTasks= new ArrayList<>();
+        if(tagName==TaskTag.FITNESS){
             colour = Color.BLUE;
             this.tag=TaskTag.FITNESS;
         }
-        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.SCHOOL))){
+        else if(tagName == TaskTag.SCHOOL){
             colour=Color.RED;
             this.tag=TaskTag.SCHOOL;
         }
-        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.WORK))){
+        else if(tagName == TaskTag.WORK){
             colour = Color.YELLOW;
             this.tag = TaskTag.WORK;
         }
-        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.APPOINTMENT))){
+        else if(tagName == TaskTag.APPOINTMENT){
             colour = Color.MAGENTA;
             this.tag = TaskTag.APPOINTMENT;
         }
-        else if(tagName.equalsIgnoreCase(String.valueOf(TaskTag.PRODUCTIVITY))){
+        else if(tagName == TaskTag.PRODUCTIVITY){
             colour = Color.CYAN;
             this.tag = TaskTag.PRODUCTIVITY;
         }
@@ -40,21 +50,69 @@ public class ListTag {
     }
 
     void addTask(Task newTask){
-        taskList.add(newTask);
+        TaskTag newTag = newTask.getTaskTag();
+        if(newTag==TaskTag.FITNESS){
+            fitnessTasks.add(newTask);
+        }
+        else if(newTag == TaskTag.SCHOOL){
+            schoolTasks.add(newTask);
+        }
+        else if(newTag == TaskTag.WORK){
+            workTasks.add(newTask);
+        }
+        else if(newTag == TaskTag.APPOINTMENT){
+            appointmentTasks.add(newTask);
+        }
+        else if(newTag == TaskTag.PRODUCTIVITY){
+            productiveTask.add(newTask);
+        }
+        else {
+            taskList.add(newTask);
+        }
     }
 
     void removeTask(Task rTask){
-        taskList.remove(rTask);
+        TaskTag newTag = rTask.getTaskTag();
+        if(newTag==TaskTag.FITNESS){
+            fitnessTasks.remove(rTask);
+        }
+        else if(newTag == TaskTag.SCHOOL){
+            schoolTasks.remove(rTask);
+        }
+        else if(newTag == TaskTag.WORK){
+            workTasks.remove(rTask);
+        }
+        else if(newTag == TaskTag.APPOINTMENT){
+            appointmentTasks.remove(rTask);
+        }
+        else if(newTag == TaskTag.PRODUCTIVITY){
+            productiveTask.remove(rTask);
+        }
+        else {
+            taskList.remove(rTask);
+        }
     }
 
-    List<Task> getTaskList(){
-        return taskList;
+    List<Task> getTaskList(TaskTag aTag){
+        if(aTag==TaskTag.FITNESS){
+            return fitnessTasks;
+        }
+        else if(aTag == TaskTag.SCHOOL){
+            return schoolTasks;
+        }
+        else if(aTag == TaskTag.WORK){
+            return workTasks;
+        }
+        else if(aTag == TaskTag.APPOINTMENT){
+            return appointmentTasks;
+        }
+        else if(aTag == TaskTag.PRODUCTIVITY){
+            return productiveTask;
+        }
+        else {
+            return taskList;
+        }
     }
-
-    TaskTag getTagList(){
-        return tag;
-    }
-
 
     int getColour(){
         return colour;

2258756fae2872a1f3445760884f66ad44bb0714
boparai1999 <boparai3@myumanitoba.ca>
2021-03-17 14:26:29 -0500

    Made editTask same as addTask for Consistency

--- app/src/main/java/com/example/myapplication/presentation/EditActivity.java
+++ app/src/main/java/com/example/myapplication/presentation/EditActivity.java
@@ -1,13 +1,22 @@
 package com.example.myapplication.presentation;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
@@ -20,6 +29,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.List;
 
 public class EditActivity extends AppCompatActivity {
@@ -32,9 +42,13 @@ public class EditActivity extends AppCompatActivity {
     private String taskTitle, taskDescription, taskDate, taskPriority;
     private final String tID = "taskID";
 
-    EditText title, description, date;
+    TextView title, description, date;
     //    ImageButton calender;
-    Button priority, status, save, cancel;
+    Button status, save, cancel;
+    Switch priority;
+    ImageButton calender;
+    Spinner dropDown;
+    ArrayAdapter<CharSequence> adapter;
 
 
 //    private AccessTask accessTask = new AccessTask();
@@ -52,49 +66,55 @@ public class EditActivity extends AppCompatActivity {
         taskTitle = currTask.getTaskTitle();
         taskDescription = currTask.getTaskDescription();
         taskDate = currTask.getTaskDate();
+        taskPriority=currTask.getPriority();
 
 
-        date = findViewById(R.id.editDate);
-
-//        calender = findViewById(R.id.datePicker);
-        title = findViewById(R.id.editTitle);
+        date = findViewById(R.id.date);
+        date.setText(taskDate);
+        title = findViewById(R.id.taskTitle);
         title.setText(currTask.getTaskTitle());
 
-        description = findViewById(R.id.editDescription);
+        description = findViewById(R.id.taskDescription);
         description.setText(currTask.getTaskDescription());
 
-        priority = findViewById(R.id.switchPrio);
+        priority =findViewById(R.id.highPriority);
+        dropDown=findViewById(R.id.dropDown);
+        calender=findViewById(R.id.datePicker);
+        if(taskPriority.equals("True"))
+            priority.setChecked(Boolean.TRUE);
+        //status = findViewById(R.id.switchStatus);
 
-        status = findViewById(R.id.switchStatus);
-
-        save = findViewById(R.id.saveBtn);
-        cancel = findViewById(R.id.cancelBtn);
+        save = findViewById(R.id.saveButton);
+        cancel = findViewById(R.id.cancelButton);
 
         taskTitle = title.getText().toString();
         taskDescription = description.getText().toString();
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDown.setAdapter(adapter);
+        calender.setOnClickListener(new View.OnClickListener() {
 
-//        calender.setOnClickListener(new View.OnClickListener() {
-//
-//            @RequiresApi(api = Build.VERSION_CODES.N)
-//
-//            @Override
-//            public void onClick(View v) {
-//                final Calendar calendar = Calendar.getInstance();
-//                mDate = calendar.get(Calendar.DATE);
-//                mMonth = calendar.get(Calendar.MONTH);
-//                mYear = calendar.get(Calendar.YEAR);
-//                DatePickerDialog datePickerDialog = new DatePickerDialog(EditActivity.this,
-//                        android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
-//                    @Override
-//                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-//                        date.setText(dayOfMonth+"-"+month+"-"+year);
-//
-//                    }
-//                },mYear,mMonth,mDate);
-//
-//                datePickerDialog.show();
-//            }
-//        });
+            @RequiresApi(api = Build.VERSION_CODES.N)
+
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                mDate = calendar.get(Calendar.DATE);
+                mMonth = calendar.get(Calendar.MONTH);
+                mYear = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(EditActivity.this,
+                        android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        date.setText(dayOfMonth+"-"+month+"-"+year);
+
+                    }
+                },mYear,mMonth,mDate);
+
+                datePickerDialog.show();
+            }
+        });
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,7 +133,7 @@ public class EditActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();

11b55a0d8afc7b8694288ec501b5d88c5a7c25aa
emily0906 <trann34@myumanitoba.ca>
2021-03-18 22:21:53 -0500

    update permission for database

--- app/src/main/java/com/example/myapplication/objects/Task.java
+++ app/src/main/java/com/example/myapplication/objects/Task.java
@@ -1,30 +1,97 @@
 package com.example.myapplication.objects;
 
-public class Task {
+import java.io.Serializable;
+
+public class Task implements Serializable {
     private final int taskId;
     private String taskDate;
     private String taskTitle;
     private String taskDescription;
     private String status;
+    private String priority;
+    private TaskTag taskTag;
+    private String taskCategory;
 
-    public Task(final int taskId, String taskTitle, String taskDescription, String taskDate){
+    public Task(int taskId,String taskTitle, String taskDescription, String taskDate){
         this.taskId = taskId;
-        if(taskTitle.length()==0)
-            this.taskTitle = "no Title";
-        else {
-            this.taskTitle = taskTitle;
-        }
+        this.taskTitle=taskTitle;
         this.taskDescription = taskDescription;
         this.taskDate = taskDate;
         this.status = "on going";
+        this.taskTag = TaskTag.OTHERS;
+        this.priority="False";
     }
+
+
+    public Task(final int taskId, String taskTitle, String taskDescription, String taskDate, String taskTag){
+        this.taskId = taskId;
+        this.taskTitle=taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskDate = taskDate;
+        this.status = "on going";
+        this.priority="False";
+        if(taskTag.equalsIgnoreCase(TaskTag.SCHOOL.toString()))
+            this.taskTag=TaskTag.SCHOOL;
+        else if(taskTag.equalsIgnoreCase(TaskTag.WORK.toString()))
+            this.taskTag=TaskTag.WORK;
+        else if(taskTag.equalsIgnoreCase(TaskTag.FITNESS.toString()))
+            this.taskTag=TaskTag.FITNESS;
+        else if(taskTag.equalsIgnoreCase(TaskTag.PRODUCTIVITY.toString()))
+            this.taskTag=TaskTag.PRODUCTIVITY;
+        else if(taskTag.equalsIgnoreCase(TaskTag.APPOINTMENT.toString()))
+            this.taskTag=TaskTag.APPOINTMENT;
+        else
+            this.taskTag=TaskTag.OTHERS;
+    }
+    public Task(final int taskId, String taskTitle, String taskDescription, String taskDate, String taskTag, String priority){
+        this.taskId = taskId;
+        this.taskTitle=taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskDate = taskDate;
+        this.status = "on going";
+        if(taskTag.equalsIgnoreCase(TaskTag.SCHOOL.toString()))
+            this.taskTag=TaskTag.SCHOOL;
+        else if(taskTag.equalsIgnoreCase(TaskTag.WORK.toString()))
+            this.taskTag=TaskTag.WORK;
+        else if(taskTag.equalsIgnoreCase(TaskTag.FITNESS.toString()))
+            this.taskTag=TaskTag.FITNESS;
+        else if(taskTag.equalsIgnoreCase(TaskTag.PRODUCTIVITY.toString()))
+            this.taskTag=TaskTag.PRODUCTIVITY;
+        else if(taskTag.equalsIgnoreCase(TaskTag.APPOINTMENT.toString()))
+            this.taskTag=TaskTag.APPOINTMENT;
+        else
+            this.taskTag=TaskTag.OTHERS;
+        this.priority=priority;
+    }
+    public Task(final int taskId, String taskTitle, String taskDescription, String taskDate,String taskTag,String status,String priority){
+        this.taskId = taskId;
+        this.taskTitle=taskTitle;
+        this.taskDescription = taskDescription;
+        this.taskDate = taskDate;
+        this.status = status;
+        this.priority=priority;
+        if(taskTag.equalsIgnoreCase(TaskTag.SCHOOL.toString()))
+            this.taskTag=TaskTag.SCHOOL;
+        else if(taskTag.equalsIgnoreCase(TaskTag.WORK.toString()))
+            this.taskTag=TaskTag.WORK;
+        else if(taskTag.equalsIgnoreCase(TaskTag.FITNESS.toString()))
+            this.taskTag=TaskTag.FITNESS;
+        else if(taskTag.equalsIgnoreCase(TaskTag.PRODUCTIVITY.toString()))
+            this.taskTag=TaskTag.PRODUCTIVITY;
+        else if(taskTag.equalsIgnoreCase(TaskTag.APPOINTMENT.toString()))
+            this.taskTag=TaskTag.APPOINTMENT;
+        else
+            this.taskTag=TaskTag.OTHERS;
+    }
+
+
 
     public int getCurrTaskId(){return taskId;}
 
     public String getTaskTitle(){return taskTitle;}
 
     public void setTaskTitle(String taskTitle){
-            this.taskTitle = taskTitle;
+        this.taskTitle = taskTitle;
     }
 
     public String getTaskDescription(){return taskDescription;}
@@ -51,5 +118,33 @@ public class Task {
             this.status = newStatus;
     }
 
+    public String getPriority() { return priority; }
 
+    public void setPriority(String priority) {
+
+        this.priority = priority;
+    }
+    public String getCategory()
+    {
+        return taskCategory;
+    }
+    public void setCategory(String taskTag)
+    {
+        taskCategory=taskTag;
+        if(taskTag.equalsIgnoreCase(TaskTag.SCHOOL.toString()))
+            this.taskTag=TaskTag.SCHOOL;
+        else if(taskTag.equalsIgnoreCase(TaskTag.WORK.toString()))
+            this.taskTag=TaskTag.WORK;
+        else if(taskTag.equalsIgnoreCase(TaskTag.FITNESS.toString()))
+            this.taskTag=TaskTag.FITNESS;
+        else if(taskTag.equalsIgnoreCase(TaskTag.PRODUCTIVITY.toString()))
+            this.taskTag=TaskTag.PRODUCTIVITY;
+        else if(taskTag.equalsIgnoreCase(TaskTag.APPOINTMENT.toString()))
+            this.taskTag=TaskTag.APPOINTMENT;
+        else
+            this.taskTag=TaskTag.OTHERS;
+    }
+
+
+    public TaskTag getTaskTag() { return taskTag; }
 }

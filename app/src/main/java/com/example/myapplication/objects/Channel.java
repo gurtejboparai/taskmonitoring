3912bbe256219d7479e78f5fcf21c6253f0b0bcb
emily0906 <trann34@myumanitoba.ca>
2021-04-12 17:49:18 -0500

    Notification upload

--- app/src/main/java/com/example/myapplication/objects/Channel.java
+++ app/src/main/java/com/example/myapplication/objects/Channel.java
@@ -11,7 +11,7 @@ import androidx.core.app.NotificationCompat;
 import com.example.myapplication.R;
 
 public class Channel extends ContextWrapper {
-    private final static String CHANNEL_ID = "task monitor channel";
+    public final static String CHANNEL_ID = "task monitor channel";
     public Channel(Context base) {
         super(base);
         createChannel();
@@ -21,19 +21,9 @@ public class Channel extends ContextWrapper {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID,"Task Monitor", NotificationManager.IMPORTANCE_DEFAULT);
             channel.setDescription("Task monitor channel for alarm");
-
             NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
         }
-    }
-
-    public NotificationCompat.Builder getChannel(String Title, String Description){
-        return new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
-                .setContentTitle(Title)
-                .setContentText(Description)
-                .setSmallIcon(R.drawable.programme)
-                .setAutoCancel(true);
-
     }
 
 
